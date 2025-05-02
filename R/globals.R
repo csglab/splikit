491f94bc1c0eba619ff6987aa1b46d114fd69b31
Arshammik <79arsham@gmail.com>
2025-05-02 15:43:44 -0400

    Adding the test

---
+++ R/globals.R
@@ -0,0 +1,16 @@
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    ".I", ".N", ".GRP", ".",
+
+    "index", "start_cor_id", "chr", "start", "strand",
+    "end_cor_id", "row_names_mtx", "sample_id",
+    "start_cor_group_id", "start_cor_group_count",
+    "end_cor_group_id", "end_cor_group_count",
+    "row_names_mtx_new", "raw_row_names_mtx",
+    "group_type", "cur_group_id", "n",
+    "end", "i", "j", "group_id", "x_tot", "x_1", "x_2",
+
+
+    "ID", "type", "gene_id", "attribute", "gene_name", "seqid"
+  ))
+}
